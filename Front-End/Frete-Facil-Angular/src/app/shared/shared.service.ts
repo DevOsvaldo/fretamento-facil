ce3ddfc0574b404bec999d2f72d5fa2f490b1d05
Osvaldo  Junior <osvaldorribeirojunior@gmail.com>
2023-12-27 11:26:52 -0300

    Shared Service
    Possivelmente será implementado no futuro.

---
+++ Front-End/Frete-Facil-Angular/src/app/shared/shared.service.ts
@@ -0,0 +1,39 @@
+
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SharedService {
+  private dadosCompartilhados = new BehaviorSubject<{
+    cargaId?: number;
+    condutorId?: number;
+  }>({});
+
+  dadosCompartilhados$ = this.dadosCompartilhados.asObservable();
+
+  atualizarDados(novosDados: { cargaId?: number; condutorId?: number }): void {
+    this.dadosCompartilhados.next(novosDados);
+  }
+
+  // Certifique-se de inicializar essas variáveis com um valor padrão se necessário
+  private condutorId: number | null = null;
+  private cargaId: number | null = null;
+
+  setCondutorId(id: number | undefined): void {
+    this.condutorId = id !== undefined ? id : null;
+  }
+
+  getCondutorId(): number | null {
+    return this.condutorId;
+  }
+
+  setCargaId(id: number | undefined): void {
+    this.cargaId = id !== undefined ? id : null;
+  }
+
+  getCargaId(): number | null {
+    return this.cargaId;
+  }
+}
