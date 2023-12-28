1a97d0dbd76eb7b157b006cf92d98ee45325f8dc
Osvaldo  Junior <osvaldorribeirojunior@gmail.com>
2023-12-28 11:33:21 -0300

    Update
    Mod detem os direitos de MOD E USER

--- Back-End/src/main/java/com/projetoJwt/auth/domain/user/User.java
+++ Back-End/src/main/java/com/projetoJwt/auth/domain/user/User.java
@@ -78,11 +78,21 @@ public class User implements UserDetails, Serializable {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN) {
+            return List.of(
+                    new SimpleGrantedAuthority("ROLE_ADMIN"),
+                    new SimpleGrantedAuthority("ROLE_MOD"),
+                    new SimpleGrantedAuthority("ROLE_USER")
+            );
+        } else if (this.role == UserRole.MOD) {
+            return List.of(
+                    new SimpleGrantedAuthority("ROLE_MOD"),
+                    new SimpleGrantedAuthority("ROLE_USER")
+            );
+        } else {
 
-        if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"),
-                new SimpleGrantedAuthority("ROLE_MOD"),
-                new SimpleGrantedAuthority("ROLE_USER"));
-        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
     }
 
     @Override
