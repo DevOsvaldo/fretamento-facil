fde770cdf8ac92e5397fe970b74b891f917d9501
Osvaldo  Junior <osvaldorribeirojunior@gmail.com>
2023-12-30 16:15:24 -0300

    Update
    Adição de um Page no find All

--- Back-End/src/main/java/com/fretamentofacil/auth/services/CargaService.java
+++ Back-End/src/main/java/com/fretamentofacil/auth/services/CargaService.java
@@ -1,13 +1,22 @@
 package com.fretamentofacil.auth.services;
 
+import com.fretamentofacil.auth.domain.dto.CargaDTO;
+import com.fretamentofacil.auth.domain.dto.CargaPageDTO;
 import com.fretamentofacil.auth.domain.model.Carga;
 import com.fretamentofacil.auth.repositories.CargaRepository;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 public class CargaService {
 
@@ -18,9 +27,26 @@ public class CargaService {
         this.cargaRepository = cargaRepository;
     }
 
-    public List<Carga> findAllCarga(){
-        return cargaRepository.findAll();
+
+    public CargaPageDTO list(@PositiveOrZero int page,@Positive @Max(100) int pageSize){
+        Page<Carga> pageCarga = cargaRepository.findAll(PageRequest.of(page, pageSize));
+        List<CargaDTO> cargas = pageCarga.getContent().stream().map(this::mapCargaDTO).collect(Collectors.toList());
+        return new CargaPageDTO(cargas,pageCarga.getTotalElements(),pageCarga.getTotalPages());
     }
+    private CargaDTO mapCargaDTO(Carga carga){
+        return  new CargaDTO(
+                carga.getId(),
+                carga.getNomeCliente(),
+                carga.getEnderecoCliente(),
+                carga.getPesoCarga(),
+                carga.getValorFrete(),
+                carga.getTipoVeiculo(),
+                carga.getSituacaoCarga()
+        );
+    }
+    /*public List<Carga> findAllCarga(){
+        return cargaRepository.findAll();
+    }*/
     public Optional<Carga> getCargaById(Long id){
         return cargaRepository.findById(id);
     }
