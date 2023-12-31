3b0f0f7cdfc20f6a359da6f66e7e875cb75c7941
Osvaldo  Junior <osvaldorribeirojunior@gmail.com>
2023-12-31 19:13:09 -0300

    Update Syntax

--- Front-End/Frete-Facil-Angular/src/app/services/token.interceptor.spec.ts
+++ Front-End/Frete-Facil-Angular/src/app/services/token.interceptor.spec.ts
@@ -1,11 +1,9 @@
-import { TestBed } from '@angular/core/testing';
 import { HttpInterceptorFn } from '@angular/common/http';
-
-import { tokenInterceptor } from './token.interceptor';
+import { TestBed } from '@angular/core/testing';
 
 describe('tokenInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
-    TestBed.runInInjectionContext(() => tokenInterceptor(req, next));
+  const interceptor: HttpInterceptorFn = (req, next) =>
+    TestBed.runInInjectionContext(() => interceptor(req, next));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -14,4 +12,10 @@ describe('tokenInterceptor', () => {
   it('should be created', () => {
     expect(interceptor).toBeTruthy();
   });
+  if (typeof window !== 'undefined') {
+    // Se estiver no navegador (cliente), então pode acessar o localStorage
+    // Se você estiver usando um serviço, também pode verificar se está no navegador antes de usar o localStorage.
+    // Exemplo: if (this.platform.isBrowser) { /* código que usa localStorage */ }
+    const token = localStorage.getItem('loginToken');
+  }
 });
