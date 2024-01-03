576d2cae49b598ce99f6547019b3aeba469c011b
Osvaldo  Junior <osvaldorribeirojunior@gmail.com>
2024-01-03 09:53:51 -0300

    Update CondutorRepository.java
    page findbydeletedfalse

--- Back-End/src/main/java/com/fretamentofacil/auth/repositories/CondutorRepository.java
+++ Back-End/src/main/java/com/fretamentofacil/auth/repositories/CondutorRepository.java
@@ -2,6 +2,8 @@ package com.fretamentofacil.auth.repositories;
 
 import com.fretamentofacil.auth.domain.model.SituacaoCondutor;
 import com.fretamentofacil.auth.domain.model.Condutor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +13,6 @@ import java.util.List;
 public interface CondutorRepository extends JpaRepository<Condutor, Long> {
     List<Condutor> findBySituacaoCondutor(SituacaoCondutor situacaoCondutor);
     List<Condutor> findByDeletedFalse();
-
+    Page<Condutor> findByDeletedFalse(Pageable pageable);
     List<Condutor> findByDeletedTrue();
 }
