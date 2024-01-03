0b74435c7213cf4e39969b76133a00ac90fa77a9
Osvaldo  Junior <osvaldorribeirojunior@gmail.com>
2024-01-03 09:50:12 -0300

    update

--- Back-End/src/main/java/com/fretamentofacil/auth/configuration/WebConfig.java
+++ Back-End/src/main/java/com/fretamentofacil/auth/configuration/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
                     .allowedOrigins("http://localhost:4200")
+                    .allowedOrigins("http://192.168.0.188:4200")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept");
         }
