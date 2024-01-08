2b9137cbd5cd9da39f3bc2f2378eaa123d44b0a0
Osvaldo  Junior <osvaldorribeirojunior@gmail.com>
2024-01-08 20:13:49 -0300

    Update CondutorDTO.java
    telefone adicionado nos parametros

--- Back-End/src/main/java/com/fretamentofacil/auth/domain/dto/CondutorDTO.java
+++ Back-End/src/main/java/com/fretamentofacil/auth/domain/dto/CondutorDTO.java
@@ -4,8 +4,8 @@ package com.fretamentofacil.auth.domain.dto;
 import com.fretamentofacil.auth.domain.model.SituacaoCondutor;
 import com.fretamentofacil.auth.domain.user.UserRole;
 
-public record CondutorDTO(Long id,String login, String password, UserRole role, String nome, String cpf,String cep,
-                          String endereco, String tipo_Veiculo , Double capacidadeVeiculo,
+public record CondutorDTO(Long id,String login, String password, UserRole role, String nome, String cpf,String telefone,
+                          String cep,String endereco, String tipo_Veiculo , Double capacidadeVeiculo,
                           SituacaoCondutor situacaoCondutor) {
 
 
